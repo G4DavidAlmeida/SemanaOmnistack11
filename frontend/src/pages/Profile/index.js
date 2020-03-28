e8710808c22ae0c4d74da3298634001575f8f7d3
David Almeida <almeida.david@gmail.com>
2020-03-28 11:47:45 -0300

    undo mistakes and screens  register, profile, login created

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile () {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+            </header>
+        </div>
+    );
+}
