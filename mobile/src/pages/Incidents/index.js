fc7a02d4daea96241ceefddf3e30736aec8285cd
G4DavidAlmeida <almeida.davidc@gmail.com>
2020-03-29 16:02:00 +0000

    mobile foder created

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from 'react-native';
+
+import styles from './styles';
+
+export default function Incidents () {
+    return (
+        <Text>Details</Text>
+    );
+}
